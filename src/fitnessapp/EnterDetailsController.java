d65a31ae8754017cf1540599a217ef1c697f0164
Unknown <andyrobertdev@gmail.com>
2018-09-27 00:20:42 -0400

    Selecting any workout will switch to the Enter Details scene and will update the title of the window to whichever workout is selected.  Back button implemented on details screen.
    TODO Next: Add functionality to enter and save workout data entered.

--- src/fitnessapp/EnterDetailsController.java
+++ src/fitnessapp/EnterDetailsController.java
@@ -5,9 +5,20 @@
  */
 package fitnessapp;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +26,40 @@ import javafx.fxml.Initializable;
  * @author andyr
  */
 public class EnterDetailsController implements Initializable {
+    
+    @FXML
+    private Text exerciseName;
+    
+    @FXML
+    private Button backButton;
+    
+    @FXML
+    private Button saveAndAddNewButton; 
+    
+    @FXML
+    private Button saveAndMenuButton;
+        
+    
+    public void backButtonAction(ActionEvent event) throws IOException{
+        Parent pickExerciseParent = FXMLLoader.load(getClass().getResource("PickExercise.fxml"));
+        Scene pickExerciseScene = new Scene(pickExerciseParent);
+        
+        //This line gets the stage info
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setTitle(((Button)event.getSource()).getText());
+        window.setScene(pickExerciseScene);
+        window.show();
+    }
+    
+    public void saveAndAddNewAction(ActionEvent event) throws IOException{
+        
+    }
+    
+    public void saveAndMenuAction(ActionEvent event) throws IOException{
+
+    }
+            
 
     /**
      * Initializes the controller class.
@@ -23,5 +68,11 @@ public class EnterDetailsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    //This method is public so it can be accessed from the previous screen.
+    //This method will set the name of the workout to reflect the button pressed
+    public void setExerciseTitle(String name) {
+        exerciseName.setText(name);
+    }
     
 }
--- src/fitnessapp/PickExerciseController.java
+++ src/fitnessapp/PickExerciseController.java
@@ -5,13 +5,20 @@
  */
 package fitnessapp;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -20,20 +27,51 @@ import javafx.scene.text.Text;
  */
 public class PickExerciseController implements Initializable {
 
-    @FXML private Text actiontarget;
+   // @FXML 
+    //private Text workoutName;
     
     @FXML
-    private Label label;
+    private Button button;
     
-    
-    @FXML protected void submitButtonAction(ActionEvent event) {
-        actiontarget.setText("Sign in button pressed");
+    /*
+    @FXML protected void exerciseSelectAction(ActionEvent event) throws IOException {
+        //actiontarget.setText("Sign in button pressed");
+        
+        Parent enterDetailsParent;
+        enterDetailsParent = FXMLLoader.load(getClass().getResource("EnterDetails.fxml"));
+        Scene enterDetailsScene = new Scene(enterDetailsParent);
+        
+        //This line gets the stage info
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setScene(enterDetailsScene);
+        window.show();
+        
+       // enterWorkoutDetailsScene(event);
+        System.out.println("Button Pressed!");
+    } 
+*/
+
+     public void exerciseSelectAction(ActionEvent event) throws IOException{
+        Parent enterDetailsParent = FXMLLoader.load(getClass().getResource("EnterDetails.fxml"));
+        Scene enterDetailsScene = new Scene(enterDetailsParent);
+        
+        //This line gets the stage info
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setTitle(((Button)event.getSource()).getText());
+        window.setScene(enterDetailsScene);
+        window.show();
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         
-    }    
+    }
+
+    private void enterWorkoutDetailsScene(ActionEvent event) throws IOException{
+
+    }
     
 }
